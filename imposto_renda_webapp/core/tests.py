a28f7777fe8bc9fa45c027c5502bceca6db8d59e
hian <hiantrader@gmail.com>
2020-07-29 17:13:43 -0300

    add novo test

--- imposto_renda_webapp/core/tests.py
+++ imposto_renda_webapp/core/tests.py
@@ -9,3 +9,9 @@ class TestCalculoImpostRenda(SimpleTestCase):
         post_data = dict(salario_anual=10000)
         response = c.post('/calcular-imposto-renda', post_data)
         self.assertEqual("Isento!", response.content.decode())
+
+    def test_calculo_salario_100000(self):
+        c = Client()
+        post_data = dict(salario_anual=100000)
+        response = c.post('/calcular-imposto-renda', post_data)
+        self.assertNotEqual("Isento!", response.content.decode())
